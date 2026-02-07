04b85814627ee7ae3b18809561be96fbe1896468
Wilberth Sanchez <wilberth2501229@hybridge.education>
2026-02-07 07:08:48 -0600

    Se creo el archivo inicial del servidor

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_restful import Api, Resource
+from rutas import crear_rutas
+
+app = Flask(__name__)
+api= Api(app)
+crear_rutas(api)
+
+
+app.run(port=8080, debug=True)
